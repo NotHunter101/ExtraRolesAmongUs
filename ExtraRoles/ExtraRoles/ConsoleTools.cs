ce377eaaa11d12ed008d1b954e69e47f872631f2
NotHunter101 <35887932+NotHunter101@users.noreply.github.com>
2021-01-31 14:49:18 -0500

    Switch the mod to Reactor
    Fixed medic report colors and switched the mod to the Among Us Reactor framework.

---
+++ ExtraRoles/ExtraRoles/ConsoleTools.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtraRolesMod
+{
+    class ConsoleTools
+    {
+        public static void Info(string message)
+        {
+            System.Console.ForegroundColor = ConsoleColor.Yellow;
+            System.Console.WriteLine("[ExtraR INFO] " + message);
+            System.Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void Error(string message)
+        {
+            System.Console.ForegroundColor = ConsoleColor.Red;
+            System.Console.WriteLine("[ExtraR ERROR] " + message);
+            System.Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
