e3c5279e1afaeea4362d170e9a234bd0d3ab61bc
NotHunter101 <35887932+NotHunter101@users.noreply.github.com>
2021-02-13 13:48:23 -0500

    Update version string

--- ExtraRoles/ExtraRoles.cs
+++ ExtraRoles/ExtraRoles.cs
@@ -117,7 +117,7 @@ namespace ExtraRolesMod
         //renderer for the shield indicator
         public static SpriteRenderer shieldRenderer = null;
         //medic settings and values
-        public static string versionString = "v1.2.6";
+        public static string versionString = "v1.2.7";
         public static class ModdedPalette
         {
             public static Color medicColor = new Color(36f / 255f, 183f / 255f, 32f / 255f, 1);
